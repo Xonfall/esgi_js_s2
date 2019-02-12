3e9d33f5e0de3aeda070d308f4fbcbeaabfd32c0
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-02-12 17:16:05 +0100

    Add many things

--- server/index.js
+++ server/index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const movieRouter = require('./routes/movies');
 const userRouter = require('./routes/users');
+const security = require('./middlewares/security');
+const securityRouter = require('./routes/security');
 const app = express();
+
+app.use(bodyparser.json());
+app.use(security.verifyToken);
+app.use('/', securityRouter);
 
 app.use('/movies', movieRouter);
 app.use('/users', userRouter);
+
 app.listen(3000, () => console.log('Listening in port 3000'));
