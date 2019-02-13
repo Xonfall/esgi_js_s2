ccb5b8d1e55e8fce872b8691b8969b0cddde1e24
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-02-13 16:52:07 +0100

    Add subscription container

---
+++ client/src/containers/SubscriptionContainer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import BannerComponent from '../components/BannerComponent';
+import ToggleButtonComponent from "../components/ToggleButtonComponent";
+
+export default class SubscriptionContainer extends Component {
+    state =  {
+        theme: "black"
+    };
+
+    toggleTheme = () => {
+        console.log(this);
+        this.setState({theme: this.state.theme === "black" ? "light" : "black"});
+        this.alertChange();
+    };
+
+    alertChange = () => alert(this.state.theme);
+    render() {
+        return (
+            <React.Fragment>
+                <ToggleButtonComponent toggleHandler={this.toggleTheme}/>
+                <BannerComponent content={this.state.theme}/>
+            </React.Fragment>
+        );
+    }
+}
