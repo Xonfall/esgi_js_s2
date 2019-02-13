5ce67e25f22feb23b4c60bf22ed27d98c83125d2
Sinicha <sinirados@gmail.com>
2019-02-13 17:01:53 +0100

    Fix Cors on Server

--- server/index.js
+++ server/index.js
@@ -4,11 +4,10 @@ const movieRouter = require('./routes/movies');
 const userRouter = require('./routes/users');
 const security = require('./middlewares/security');
 const securityRouter = require('./routes/security');
+const app = express();
 const cors = require('cors');
 
-const app = express();
-
-app.use(cors);
+app.use(cors());
 
 app.use(bodyparser.json());
 app.use(security.verifyToken);
@@ -18,3 +17,4 @@ app.use('/movies', movieRouter);
 app.use('/users', userRouter);
 
 app.listen(3000, () => console.log('Listening in port 3000'));
+
