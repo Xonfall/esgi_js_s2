c40908634ad05f643f85f54a9c991485322a3259
Jérémy Bachayani <jeremy.bachayani@gmail.com>
2019-02-13 16:42:41 +0100

    Add LoginForm

---
+++ client/src/containers/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+export default class LoginForm extends Component {
+    state = {
+        username: '',
+        password: ''
+    };
+
+    handleKeyUp = (event, field) => {
+        const input = event.currentTarget;
+
+        this.setState({
+            [field]: input.value
+        })
+    };
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        this.props.onSubmit(this.state)
+    };
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>Username</label>
+                <input onKeyUp={(event) => this.handleKeyUp(event, "username")}/>
+                <label>Password</label>
+                <input type="password" onKeyUp={(event) => this.handleKeyUp(event, 'password')}/>
+                <button type="submit">Submit</button>
+            </form>
+        )
+    }
+}
