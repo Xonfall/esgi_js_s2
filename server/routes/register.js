ee525c66b5c92c6f5ae8fa89aa9d89591bae8c5a
Xonfall <xonfall@live.fr>
2019-02-14 15:44:29 +0100

    Add dynamic login

--- server/routes/register.js
+++ server/routes/register.js
@@ -3,30 +3,24 @@ const User = require('../models/user');
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    console.log(req.body);
     if (
         req.body.firstName !== null &&
         req.body.lastName !== null &&
         req.body.email !== null &&
         req.body.password !== null
         ) {
-            const result = User.findOne({email: req.body.email},);
-
+            const result = User.findOne({email: req.body.email});
             if (result.email != null) {
                 res.send('Email utilisé')
             } else {
-            const user = new User();
-            user.firstName = req.body.firstName;
-            user.lastName = req.body.lastName;
-            user.email = req.body.email;
-            user.password = req.body.password;
-            user.created_at = Date();
+                const {firstName, lastName, email, password} = req.body;
+                const user = new User({firstName, lastName, email, password, created_at: Date()});
 
-            user.save( function (error, result){
-                console.log(error);
-                console.log(result)
-                res.send(`Félicitation ${user.firstName} vous êtes inscrit`);
-            })
+                user.save( function (error, result){
+                    console.log(error);
+                    console.log(result)
+                    res.send(`Félicitation ${user.firstName} vous êtes inscrit`);
+                })
         }
     }
 });
